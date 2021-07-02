d52cf6668777029ddb4396b34b076cfa723d6381
hkopanski <khalid.kopanski@gmail.com>
2021-07-02 14:12:02 -0400

    render file script updated with Rmd name

--- render_files.R
+++ render_files.R
@@ -1,5 +1,6 @@
 day_list <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 
 for(i in day_list){
-  rmarkdown::render("<file.Rmd>", params = list(day_of_week = i))
+  rmarkdown::render("ST558 Project 2.Rmd", params = list(day_of_week = i),
+                    output_file = paste0("Report-", i))
 }
